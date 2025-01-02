939dda60ddbcc42b2db0409cbf3b67ffc5c3f016
Kyrychenko Nikita <nikdevrych@gmail.com>
2025-01-02 20:10:35 +0200

    3 setup project (#4)
    * #3 remove redundant files where been created in initial commit
    
    * #3 add prettier and setup it
    
    * #3 add material ui, setup it, fix dark mode flicker
    
    * #3 update layout metadata

---
+++ src/theme.ts
@@ -0,0 +1,17 @@
+"use client";
+
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  colorSchemes: {
+    dark: true,
+  },
+  cssVariables: {
+    colorSchemeSelector: "class",
+  },
+  typography: {
+    fontFamily: "var(--font-roboto)",
+  },
+});
+
+export default theme;
